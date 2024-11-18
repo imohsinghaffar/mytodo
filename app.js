0fb4d2fd9bf524b42c1eac3f027c784573195dc7
imohsinghaffar <mohsinghaffar546@gmail.com>
2024-11-18 19:17:57 +0100

    Built TodoLis using JS,HTML, CSS

---
+++ app.js
@@ -0,0 +1,37 @@
+let getInput = document.querySelector(".input");
+let btnClick = document.querySelector(".btn");
+let accessUl = document.querySelector('.ul');
+function todo() {
+    if (getInput.value) {
+        let createlist = document.createElement("li");
+        createlist.innerHTML = getInput.value.toUpperCase();
+        createlist.setAttribute("class", "li-style newEle");
+        accessUl.appendChild(createlist)
+
+        let deleteBtn = document.createElement('button');
+        deleteBtn.innerHTML = 'Delete';
+        deleteBtn.setAttribute('class','delBtn')
+
+        createlist.appendChild(deleteBtn);
+        getInput.value = "";
+
+        deleteBtn.addEventListener('click', () => {
+            accessUl.removeChild(createlist);
+            if(deleteBtn.event.key==="mouseover")
+            {
+                document.write("Success");
+            }
+        })
+
+    } else {
+        alert("There is something wrong!");
+    }
+}
+
+btnClick.addEventListener("click", todo)
+getInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+        todo()
+    }
+})
+
